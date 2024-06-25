d66cdb37b955d92f9c0599c19409867fea9bcfff
Taylor <115taegyeong@gmail.com>
2024-06-25 21:10:17 +0900

    feat: add keystore

--- src/main/java/io/taylor/Application.java
+++ src/main/java/io/taylor/Application.java
@@ -6,22 +6,49 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @SpringBootApplication
+@RestController
 public class Application {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring";
+    }
+
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(args);
+//        SpringApplication application = new SpringApplication(Application.class);
+//        application.setWebApplicationType(WebApplicationType.NONE);
+//        application.run(args);
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
     }
 }
 
