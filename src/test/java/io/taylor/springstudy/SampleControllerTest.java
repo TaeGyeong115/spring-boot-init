9885482ea55ef4e7c1aacfab0a3e004b76dec732
Taylor <115taegyeong@gmail.com>
2024-06-26 19:30:41 +0900

    feat: outputCapture

--- src/test/java/io/taylor/springstudy/SampleControllerTest.java
+++ src/test/java/io/taylor/springstudy/SampleControllerTest.java
@@ -2,13 +2,18 @@ package io.taylor.springstudy;
 
 import io.taylor.sample.SampleController;
 import io.taylor.sample.SampleService;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -24,26 +29,47 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK) // 통합 테스트
 //@AutoConfigureMockMvc
-//@WebMvcTest(SampleController.class) // slice test
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(SampleController.class) // slice test
+//@ExtendWith(OutputCaptureExtension.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SampleControllerTest {
-    @Autowired
-    WebTestClient webTestClient;
 
     @MockBean
-    SampleService mockSampleService;
     // 사용하는 게 있다면 빈을 주입해야 함
+    SampleService mockSampleService;
 
-    // rest call
+    @Autowired
+    MockMvc mockMvc;
+
+    @Rule
+    public OutputCaptureRule outputCapture = new OutputCaptureRule();
+
     @Test
     public void hello() throws Exception {
         when(mockSampleService.getName()).thenReturn("taylor");
 
-        webTestClient.get().uri("/hello")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class).isEqualTo("hello taylor");
+        mockMvc.perform(get("/hello")).andExpect(content().string("hello taylor"));
+
+        // log massage 확인
+        assertThat(outputCapture.toString())
+                .contains("holoman")
+                .contains("skip");
     }
+
+// rest call
+//    @Autowired
+//    WebTestClient webTestClient;
+//
+//    @Test
+//    public void hello() throws Exception {
+//        when(mockSampleService.getName()).thenReturn("taylor");
+//
+//        webTestClient.get().uri("/hello")
+//                .exchange()
+//                .expectStatus().isOk()
+//                .expectBody(String.class).isEqualTo("hello taylor");
+//        assertThat(outputCapture.toString());
+//    }
 
 //    @Autowired
 //    TestRestTemplate testRestTemplate;
